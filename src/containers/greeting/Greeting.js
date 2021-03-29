5e3934577b9fc5e2431f76a43af48e6e3ede2c92
gururamu4 <44159423+gururamu4@users.noreply.github.com>
2021-03-30 00:48:19 +0530

    Update Greeting.js

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -6,7 +6,6 @@ import landingPerson from "../../assets/lottie/landingPerson";
 import DisplayLottie from "../../components/displayLottie/DisplayLottie";
 import SocialMedia from "../../components/socialMedia/SocialMedia";
 import Button from "../../components/button/Button";
-import image from '../../assets/images/snapchat-guru.svg'
 import {illustration, greeting} from "../../portfolio";
 import StyleContext from "../../contexts/StyleContext";
 
