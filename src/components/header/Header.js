286914113e48bae1862deec7fbb59fa427732e03
gurutricon <guru.ramu@triconinfotech.com>
2021-03-30 00:09:14 +0530

    initial push

---
+++ src/components/header/Header.js
@@ -0,0 +1,94 @@
+import React, {useContext} from "react";
+import Headroom from "react-headroom";
+import "./Header.css";
+import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
+import StyleContext from "../../contexts/StyleContext";
+import {
+  greeting,
+  workExperiences,
+  skillsSection,
+  openSource,
+  blogSection,
+  talkSection,
+  achievementSection,
+  bigProjects
+} from "../../portfolio";
+import image from '../../assets/images/snapchat-guru.svg'
+
+function Header() {
+  const {isDark} = useContext(StyleContext);
+  const viewExperience = workExperiences.display;
+  const viewOpenSource = openSource.display;
+  const viewSkills = skillsSection.display;
+  const viewAchievement = achievementSection.display;
+  const bigProject = bigProjects.display;
+  const viewBlog = blogSection.display;
+  const viewTalks = talkSection.display;
+
+  return (
+    <Headroom>
+      <header className={isDark ? "dark-menu header" : "header"}>
+        <a href="/" className="logo">
+          <span className="grey-color"> &lt;</span>
+          <span className="logo-name">{greeting.username}</span>
+          <span className="grey-color">/&gt;</span>
+          {/* <img style={{height: 48, width: 48}} src={image}/> */}
+        </a>
+        <input className="menu-btn" type="checkbox" id="menu-btn" />
+        <label
+          className="menu-icon"
+          htmlFor="menu-btn"
+          style={{color: "white"}}
+        >
+          <span className={isDark ? "navicon navicon-dark" : "navicon"}></span>
+        </label>
+        <ul className={isDark ? "dark-menu menu" : "menu"}>
+          {viewSkills && (
+            <li>
+              <a href="#skills">Skills</a>
+            </li>
+          )}
+          {bigProject && (
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+          )}
+          {viewExperience && (
+            <li>
+              <a href="#experience">Work Experiences</a>
+            </li>
+          )}
+          {viewOpenSource && (
+            <li>
+              <a href="#opensource">Open Source</a>
+            </li>
+          )}
+          {viewAchievement && (
+            <li>
+              <a href="#achievements">Achievements</a>
+            </li>
+          )}
+          {viewBlog && (
+            <li>
+              <a href="#blogs">Blogs</a>
+            </li>
+          )}
+          {viewTalks && (
+            <li>
+              <a href="#talks">Talks</a>
+            </li>
+          )}
+          <li>
+            <a href="#contact">Contact Me</a>
+          </li>
+          <li>
+            <a href="#">
+              <ToggleSwitch />
+            </a>
+          </li>
+        </ul>
+      </header>
+    </Headroom>
+  );
+}
+export default Header;
